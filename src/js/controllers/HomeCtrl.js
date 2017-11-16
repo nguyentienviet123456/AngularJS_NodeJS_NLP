a09649f98e3eb1f16504b15444568868ddba41dd
nguyentienviet123456 <nguyentienviet123456@gmail.com>
2017-11-16 11:47:54 +0700

    finish ui

--- src/js/controllers/HomeCtrl.js
+++ src/js/controllers/HomeCtrl.js
@@ -2,268 +2,147 @@ angular.module('helloWorldApp')
 .controller('HomeCtrl', [
     '$scope',
     function($scope) {
-      $scope.test = "test";
-        $scope.srcImage = "./images/mic.gif";
-        // start region langs
-        var langs =
-        [['Afrikaans',       ['af-ZA']],
-         ['Bahasa Indonesia',['id-ID']],
-         ['Bahasa Melayu',   ['ms-MY']],
-         ['Català',          ['ca-ES']],
-         ['Čeština',         ['cs-CZ']],
-         ['Deutsch',         ['de-DE']],
-         ['VietNam',        ['vi-VI']],
-         ['English',         ['en-AU', 'Australia'],
-                             ['en-CA', 'Canada'],
-                             ['en-IN', 'India'],
-                             ['en-NZ', 'New Zealand'],
-                             ['en-ZA', 'South Africa'],
-                             ['en-GB', 'United Kingdom'],
-                             ['en-US', 'United States']],
-         ['Español',         ['es-AR', 'Argentina'],
-                             ['es-BO', 'Bolivia'],
-                             ['es-CL', 'Chile'],
-                             ['es-CO', 'Colombia'],
-                             ['es-CR', 'Costa Rica'],
-                             ['es-EC', 'Ecuador'],
-                             ['es-SV', 'El Salvador'],
-                             ['es-ES', 'España'],
-                             ['es-US', 'Estados Unidos'],
-                             ['es-GT', 'Guatemala'],
-                             ['es-HN', 'Honduras'],
-                             ['es-MX', 'México'],
-                             ['es-NI', 'Nicaragua'],
-                             ['es-PA', 'Panamá'],
-                             ['es-PY', 'Paraguay'],
-                             ['es-PE', 'Perú'],
-                             ['es-PR', 'Puerto Rico'],
-                             ['es-DO', 'República Dominicana'],
-                             ['es-UY', 'Uruguay'],
-                             ['es-VE', 'Venezuela']],
-         ['Euskara',         ['eu-ES']],
-         ['Français',        ['fr-FR']],
-         ['Galego',          ['gl-ES']],
-         ['Hrvatski',        ['hr_HR']],
-         ['IsiZulu',         ['zu-ZA']],
-         ['Íslenska',        ['is-IS']],
-         ['Italiano',        ['it-IT', 'Italia'],
-                             ['it-CH', 'Svizzera']],
-         ['Magyar',          ['hu-HU']],
-         ['Nederlands',      ['nl-NL']],
-         ['Norsk bokmål',    ['nb-NO']],
-         ['Polski',          ['pl-PL']],
-         ['Português',       ['pt-BR', 'Brasil'],
-                             ['pt-PT', 'Portugal']],
-         ['Română',          ['ro-RO']],
-         ['Slovenčina',      ['sk-SK']],
-         ['Suomi',           ['fi-FI']],
-         ['Svenska',         ['sv-SE']],
-         ['Türkçe',          ['tr-TR']],
-         ['български',       ['bg-BG']],
-         ['Pусский',         ['ru-RU']],
-         ['Српски',          ['sr-RS']],
-         ['한국어',            ['ko-KR']],
-         ['中文',             ['cmn-Hans-CN', '普通话 (中国大陆)'],
-                             ['cmn-Hans-HK', '普通话 (香港)'],
-                             ['cmn-Hant-TW', '中文 (台灣)'],
-                             ['yue-Hant-HK', '粵語 (香港)']],
-         ['日本語',           ['ja-JP']],
-         ['Lingua latīna',   ['la']]];
-        
-        // region define function
-        var updateCountry = function() {
-          for (var i = select_dialect.options.length - 1; i >= 0; i--) {
-              select_dialect.remove(i);
-            }
-            var list = langs[select_language.selectedIndex];
-            for (var i = 1; i < list.length; i++) {
-              select_dialect.options.add(new Option(list[i][1], list[i][0]));
-            }
-            select_dialect.style.visibility = list[1].length == 1 ? 'hidden' : 'visible';
+      $scope.srcImage = "images/mic.gif";
+      //startregion variable
+      var ignore_onend;
+      var start_timestamp;
+      var recognizing;
+      $scope.final_transcript = '';
+      var create_email;
+      $scope.infoStart = false;
+      $scope.infoSpeachNow = false;
+      $scope.infoNoSpeech = false;
+      $scope.infoNoMicroPhone = false;
+      $scope.infoAllow = false;
+      $scope.infoDenied = false;
+      $scope.infoBlocled = false;
+      $scope.infoUpgrade = false;
+      $scope.final_span = "start";
+      $scope.interim_span = "START";
+      //endregion
+
+      //startregion define function 
+      function upgrade() {
+        // angular.element("#start_button").style.visibility = 'hidden';
+        $scope.infoUpgrade = true;
+      }
+
+      var two_line = /\n\n/g;
+      var one_line = /\n/g;
+      function linebreak(s) {
+        return s.replace(two_line, '<p></p>').replace(one_line, '<br>');
+      }
+      
+      var first_char = /\S/;
+      function capitalize(s) {
+        return s.replace(first_char, function(m) { return m.toUpperCase(); });
+      }
+
+     
+      //endregion
+
+      if(!('webkitSpeechRecognition' in window)){
+        upgrade();
+      }else{
+        // angular.element("start_button").style.display = "inline-block";
+        var recognition = new webkitSpeechRecognition();
+        recognition.continuous = true;
+        recognition.interimResults = true;
+
+        recognition.onstart = function (){
+          recognizing = true;
+          $scope.infoSpeachNow = true;
+          $scope.srcImage = "images/mic-slash.gif";
         };
 
-        var upgrade = function() {
-          start_button.style.visibility = 'hidden';
-          showInfo('info_upgrade');
-        }
+        recognition.onerror = function(event){
+          if(event.error = "no-speech"){
+            $scope.srcImage = "images/mic.gif";
+            $scope.infoNoSpeech = true;
+            ignore_onend = true;
+          }
+          if(event.error = "audio-captrue"){
+            $scope.srcImage = "images/mic.gif";
+            $scope.infoNoMicroPhone = true;
+            ignore_onend = true;
+          }
+          if(event.error = "not-allowed"){
+            if(event.timeStamp - start_timestamp < 100){
+              $scope.infoBlocked = true;
+            }else{
+              $scope.Denied = true;
+            }
+            ignore_onend = true;
+          }
+        };
 
+        recognition.onend = function (){
+          recognizing = false;
+          if(ignore_onend){
+            return;
+          }
+          $scope.srcImage= "images/mic.gif";
+          if(!$scope.final_transcript){
+            $scope.infoStart = true;
+            return;
+          }
+          // TODO: set false all show
+          $scope.infoStart = false;
+          $scope.infoSpeachNow = false;
+          $scope.infoNoSpeech = false;
+          $scope.infoNoMicroPhone = false;
+          $scope.infoAllow = false;
+          $scope.infoDenied = false;
+          $scope.infoBlocled = false;
+          $scope.infoUpgrade = false;
+          // endTODO
+          if(window.getSelection){
+            window.getSelection().removeAllRanges();
+            var range = document.createRange();
+            range.selectNode(document.getElementById("final_span"));
+            window.getSelection().addRange(range);
+          }
+        };
+
+        recognition.onresult = function(event){
+          var interim_transcript = '';
+          for(var i = event.resultIndex; i< event.results.length; i++){
+            if(event.results[i].isFinal){
+              $scope.final_transcript += event.results[i][0].transcript;
+             
+            }else{
+              interim_transcript += event.results[i][0].transcript;
+            }
+          }
+          $scope.final_transcript = capitalize($scope.final_transcript);
+          $scope.final_span = linebreak($scope.final_transcript);
+          $scope.interim_span = linebreak(interim_transcript);
+          $("#final_span").html($scope.final_transcript);
          
-        var two_line = /\n\n/g;
-        var one_line = /\n/g;
-        var linebreak =  function (s) {
-          return s.replace(two_line, '<p></p>').replace(one_line, '<br>');
-        }
-        
-        var first_char = /\S/;
-        var capitalize = function (s) {
-          return s.replace(first_char, function(m) { return m.toUpperCase(); });
-        }
-        
-        var createEmail = function() {
-          var n = final_transcript.indexOf('\n');
-          if (n < 0 || n >= 80) {
-            n = 40 + final_transcript.substring(40).indexOf(' ');
+          console.log($scope.interim_span);
+          if($scope.final_transcript || interim_transcript){
+            $scope.inlineBlock = true;
           }
-          var subject = encodeURI(final_transcript.substring(0, n));
-          var body = encodeURI(final_transcript.substring(n + 1));
-          window.location.href = 'mailto:?subject=' + subject + '&body=' + body;
-        }
-        
-        $scope.copyButton= function() {
-          if (recognizing) {
-            recognizing = false;
-            recognition.stop();
-          }
-          copy_button.style.display = 'none';
-          copy_info.style.display = 'inline-block';
-          showInfo('');
-        }
-        
-        $scope.emailButton =  function() {
-          if (recognizing) {
-            create_email = true;
-            recognizing = false;
-            recognition.stop();
-          } else {
-            createEmail();
-          }
-          email_button.style.display = 'none';
-          email_info.style.display = 'inline-block';
-          showInfo('');
-        }
-        
-        $scope.startButton = function ( event) {
-          if (recognizing) {
+        };
+
+        $scope.startButton = function(event){
+          if(recognizing){
             recognition.stop();
             return;
           }
-          final_transcript = '';
-          recognition.lang = 'vi-VI';
+          $scope.final_transcript = '';
+          recognition.lang= "vi-VI";
           recognition.start();
           ignore_onend = false;
-          final_span.innerHTML = '';
-          interim_span.innerHTML = '';
-          $scope.srcImage = './images/mic-slash.gif';
-          showInfo('info_allow');
-          showButtons('none');
+          $scope.final_span = '';
+          $("#final_span").html('');
+         
+          $scope.interim_span = '';
+          $scope.srcImage = "images/mic-animate.gif";
+          $scope.infoAllow = true;
+          $scope.showBUtton = false;
           start_timestamp = event.timeStamp;
         }
-        
-        var showInfo = function(s) {
-          if (s) {
-            for (var child = info.firstChild; child; child = child.nextSibling) {
-              if (child.style) {
-                child.style.display = child.id == s ? 'inline' : 'none';
-              }
-            }
-            info.style.visibility = 'visible';
-          } else {
-            info.style.visibility = 'hidden';
-          }
-        }
-        
-        var current_style;
-        var showButtons = function(style) {
-          if (style == current_style) {
-            return;
-          }
-          current_style = style;
-          copy_button.style.display = style;
-          email_button.style.display = style;
-          copy_info.style.display = 'none';
-          email_info.style.display = 'none';
-        }
-        // end region
-        for (var i = 0; i < langs.length; i++) {
-          select_language.options[i] = new Option(langs[i][0], i);
-        }
-        select_language.selectedIndex = 6;
-        
-        updateCountry();
-        select_dialect.selectedIndex = 6;
-        showInfo('info_start');
-        
-        // end region langs
-        $scope.updateCountry = updateCountry();
-        var create_email = false;
-        var final_transcript = '';
-        var recognizing = false;
-        var ignore_onend;
-        var start_timestamp;
-        if (!('webkitSpeechRecognition' in window)) {
-          upgrade();
-        } else {
-          start_button.style.display = 'inline-block';
-          var recognition = new webkitSpeechRecognition();
-          recognition.continuous = true;
-          recognition.interimResults = true;
-        
-          recognition.onstart = function() {
-            recognizing = true;
-            showInfo('info_speak_now');
-            $scope.srcImage = './images/mic-animate.gif';
-          };
-        
-          recognition.onerror = function(event) {
-            if (event.error == 'no-speech') {
-              $scope.srcImage = './images/mic.gif';
-              showInfo('info_no_speech');
-              ignore_onend = true;
-            }
-            if (event.error == 'audio-capture') {
-              $scope.srcImage = './imgaes/mic.gif';
-              showInfo('info_no_microphone');
-              ignore_onend = true;
-            }
-            if (event.error == 'not-allowed') {
-              if (event.timeStamp - start_timestamp < 100) {
-                showInfo('info_blocked');
-              } else {
-                showInfo('info_denied');
-              }
-              ignore_onend = true;
-            }
-          };
-        
-          recognition.onend = function() {
-            recognizing = false;
-            if (ignore_onend) {
-              return;
-            }
-            $scope.srcImage = './images/mic.gif';
-            if (!final_transcript) {
-              showInfo('info_start');
-              return;
-            }
-            showInfo('');
-            if (window.getSelection) {
-              window.getSelection().removeAllRanges();
-              var range = document.createRange();
-              range.selectNode(document.getElementById('final_span'));
-              window.getSelection().addRange(range);
-            }
-            if (create_email) {
-              create_email = false;
-              createEmail();
-            }
-          };
-        
-          recognition.onresult = function(event) {
-            var interim_transcript = '';
-            for (var i = event.resultIndex; i < event.results.length; ++i) {
-              if (event.results[i].isFinal) {
-                final_transcript += event.results[i][0].transcript;
-              } else {
-                interim_transcript += event.results[i][0].transcript;
-              }
-            }
-            final_transcript = capitalize(final_transcript);
-            final_span.innerHTML = linebreak(final_transcript);
-            interim_span.innerHTML = linebreak(interim_transcript);
-            if (final_transcript || interim_transcript) {
-              showButtons('inline-block');
-            }
-          };
-        }
+      }
     }
 ]);
